9263579a532aa20aadc9d3fb20747b8e4eb02bc2
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2025-01-24 13:56:12 +0800

    Delete 2048-PANTONE-gh-pages directory

--- 2048-PANTONE-gh-pages/js/application.js
+++
@@ -1,8 +0,0 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  game_manager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
-
-function handle_undo() {
-  game_manager.move(-1);
-}
